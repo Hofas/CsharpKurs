5f4d1f2f493dc7444ccb1b959b16f3dbf14138a9
Hofas <hafas@poczta.onet.pl>
2018-09-20 17:48:16 +0200

    lekcja 7 klasy, stworzenie klasy z jednym polem i  4 metodami

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        //pola
+        List<float> raitings;
+        //zachowania
+        /// <summary>
+        /// Dodaje kolejne oceny
+        /// </summary>
+        /// <param name="raiting"></param>
+        public void AddRaiting(float raiting)
+        {
+            raitings.Add(raiting);
+        }
+        /// <summary>
+        /// Oblicza średnią ocen
+        /// </summary>
+        /// <returns></returns>
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+            foreach (var raiting in raitings)
+            {
+                sum += raiting;
+            }
+            avg = sum / raitings.Count();
+
+            return avg;
+
+
+        }
+        /// <summary>
+        /// Wyciąga Najwyższą ocene
+        /// </summary>
+        /// <returns></returns>
+        public float GiveMaxRating()
+        {
+            return raitings.Max();
+        }
+        /// <summary>
+        /// Wyciąga Najniższą ocene
+        /// </summary>
+        /// <returns></returns>
+        public float GiveMinRating()
+        {
+            return raitings.Min();
+        }  
+
+    }
+}
