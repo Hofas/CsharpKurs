f60dc611b7a1171d2cd4435a95338dbadb98b0f5
Hofas <hafas@poczta.onet.pl>
2018-09-22 22:02:39 +0200

    Lekaca 10 zmiene i obiekty

---
+++ Odcinek10/Diary.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek10
+{
+    class Diary
+    {
+        public Diary()
+        {
+            raitings = new List<float>();
+        }
+        //pola
+        List<float> raitings;
+        //zachowania
+        /// <summary>
+        /// Dodaje kolejne oceny
+        /// </summary>
+        /// <param name="raiting"></param>
+        public void AddRaiting(float raiting)
+        {
+            raitings.Add(raiting);
+        }
+        /// <summary>
+        /// Oblicza średnią ocen
+        /// </summary>
+        /// <returns></returns>
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+            foreach (var raiting in raitings)
+            {
+                sum += raiting;
+            }
+            avg = sum / raitings.Count();
+
+            return avg;
+
+
+        }
+        /// <summary>
+        /// Wyciąga Najwyższą ocene
+        /// </summary>
+        /// <returns></returns>
+        public float GiveMaxRating()
+        {
+            return raitings.Max();
+        }
+        /// <summary>
+        /// Wyciąga Najniższą ocene
+        /// </summary>
+        /// <returns></returns>
+        public float GiveMinRating()
+        {
+            return raitings.Min();
+        }
+        /// <summary>
+        /// Czyścci Tablicę ocen
+        /// </summary>
+        public void ClearDiary()
+        {
+            raitings.Clear();
+
+        }
+        /// <summary>
+        /// Zlicza Oceny
+        /// </summary>
+        public float Count()
+        {
+            return raitings.Count();
+
+        }
+    }
+}
---
+++ Odcinek10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            diary.AddRaiting(1);
+            diary.AddRaiting(2);
+            diary = new Diary();
+
+        }
+
+
+    }
+}
